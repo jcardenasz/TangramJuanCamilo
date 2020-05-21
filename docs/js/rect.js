448a1533ca994740e07d2babfe466ff551e18ef1
nakednous <nakednous@gmail.com>
2020-05-21 10:17:06 -0500

    rect added as a regular file

--- docs/js/rect.js
+++ docs/js/rect.js
@@ -1,1 +1,19 @@
-../../TangramJS/rect.js
+class Rect extends Shape {
+  constructor () {
+    super();
+    this.edge = 100;
+  }
+
+  drawShape () {
+    rectMode(CENTER);
+    rect(0, 0, this.edge, this.edge);
+  }
+
+  get edge () {
+    return this._edge;
+  }
+
+  set edge (value) {
+    this._edge = value;
+  }
+}
