c32027756c36668ffa231a0ce4990db66f5309d8
nakednous <nakednous@gmail.com>
2020-05-21 08:04:36 -0500

    stand-alone js added

---
+++ TangramJS/rect.js
@@ -0,0 +1,19 @@
+class Rect extends Shape {
+  constructor () {
+    super();
+    this.edge = 100;
+  }
+
+  drawShape () {
+    rectMode(CENTER);
+    rect(0, 0, this.edge, this.edge);
+  }
+
+  get edge () {
+    return this._edge;
+  }
+
+  set edge (value) {
+    this._edge = value;
+  }
+}
